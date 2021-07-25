a3e7b34faa3957ba1b6c3508767d7e3f52e9603e
Mr-Sean <86028295+Mr-Sean@users.noreply.github.com>
2021-07-25 01:49:43 -0700

    Languages

--- 03-java-spring/02-spring-data-i/03-languages/src/main/java/com/sean/languages/controllers/LanguageController.java
+++ 03-java-spring/02-spring-data-i/03-languages/src/main/java/com/sean/languages/controllers/LanguageController.java
@@ -5,9 +5,12 @@ import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import com.sean.languages.models.Language;
 import com.sean.languages.services.LanguageService;
@@ -33,6 +36,35 @@ public class LanguageController {
 			return "index.jsp";
 		}
 		this.lService.createLanguage(language);
+		return "redirect:/";
+	}
+	
+	@GetMapping("/show/{id}")
+	public String showLang(Model model, @PathVariable("id") Long id) {
+		model.addAttribute("thislang", this.lService.findLanguage(id));
+		return "show.jsp";
+	}
+
+	
+	@GetMapping("/edit/{id}")
+	public String editLang(Model model, @PathVariable("id") Long id) {
+		model.addAttribute("language", this.lService.findLanguage(id));
+		return "edit.jsp";
+	}
+	
+	@PutMapping("/edit/{id}")
+	public String editLang(@Valid @ModelAttribute("language") Language lang, BindingResult result, Model model, @PathVariable("id") Long id) {
+		if(result.hasErrors()) {
+			model.addAttribute("language", this.lService.findLanguage(id));
+			return "edit.jsp";
+		}
+		this.lService.updateLanguage(lang);
+		return "redirect:/show/{id}";
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public String deleteLang(@PathVariable("id") Long id) {
+		this.lService.deleteLanguage(id);
 		return "redirect:/";
 	}
 
--- 03-java-spring/02-spring-data-i/03-languages/src/main/webapp/WEB-INF/show.jsp
+++ 03-java-spring/02-spring-data-i/03-languages/src/main/webapp/WEB-INF/show.jsp
@@ -5,9 +5,19 @@
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<title>Details</title>
 </head>
 <body>
+<a href="/">Dashboard</a>
 
+<p><c:out value="${thislang.name}"/></p>
+<p><c:out value="${thislang.creator}"/></p>
+<p><c:out value="${thislang.currentVersion}"/></p>
+
+<p><a href="/edit/${thislang.id}">Edit</a>
+<form method="POST" action="/delete/${thislang.id}">
+<input type="hidden" name="_method" value="delete">
+<button>Delete Language</button>
+</form>
 </body>
 </html>
