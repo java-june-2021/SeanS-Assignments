4419a2820cf0ef2a1bf3dc9fe29529fbdda70081
Mr-Sean <86028295+Mr-Sean@users.noreply.github.com>
2021-07-27 00:08:46 -0700

    Prod&Cats

---
+++ 03-java-spring/03-spring-data-ii/05-products-and-categories/src/main/java/com/sean/prodcats/repositories/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.sean.prodcats.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sean.prodcats.models.Category;
+import com.sean.prodcats.models.Product;
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Long> {
+	List<Product> findAll();
+	List<Product> findByCategoriesNotContains(Category category);
+	
+
+}
